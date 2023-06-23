b6e6580f1df16cf728e811597fc7ce63cd0830a9
udithkavinda <udith@fcodelabs.com>
2023-06-23 11:53:27 +0530

    unnecessary files removed

--- train-php-novel-T5.py
+++ train-php-novel-T5.py
@@ -60,172 +60,206 @@ class CustomDataset(Dataset):
             'target_ids_y': target_ids.to(dtype=torch.long)
         }
 
-class GeneratorDatasetForMultiSource(Dataset):
-    def __init__(self, dataframe, tokenizer, source_len, summ_len):
-        self.tokenizer = tokenizer
-        self.data = dataframe
-        self.source_len = source_len
-        self.summ_len = summ_len
-        self.text_data_1 = self.data.additional_info
-        self.text_data_2 = self.data.buggy
-        self.labels = self.data.patch
-        self.bugid = self.data.bugid
-        self.bug = self.data.bug
+# class GeneratorDatasetForMultiSource(Dataset):
+#     def __init__(self, dataframe, tokenizer, source_len, summ_len):
+#         self.tokenizer = tokenizer
+#         self.data = dataframe
+#         self.source_len = source_len
+#         self.summ_len = summ_len
+#         self.text_data_1 = self.data.additional_info
+#         self.text_data_2 = self.data.buggy
+#         self.labels = self.data.patch
+#         self.bugid = self.data.bugid
+#         self.bug = self.data.bug
 
-    def __len__(self):
-        return len(self.text_data_1)
+#     def __len__(self):
+#         return len(self.text_data_1)
 
-    def __getitem__(self, index):
-        text_1 = self.text_data_1[index]
-        text_2 = self.text_data_2[index]
-        label = self.labels[index]
+#     def __getitem__(self, index):
+#         text_1 = self.text_data_1[index]
+#         text_2 = self.text_data_2[index]
+#         label = self.labels[index]
 
-        # Tokenize text inputs
-        text_input_1 = self.tokenizer.batch_encode_plus([text_1], max_length= self.source_len, pad_to_max_length=True,return_tensors='pt', truncation=True)
-        text_input_2 = self.tokenizer.batch_encode_plus([text_2], max_length= self.source_len, pad_to_max_length=True,return_tensors='pt', truncation=True)
-        target = self.tokenizer.batch_encode_plus([label], max_length= self.summ_len, pad_to_max_length=True,return_tensors='pt', truncation=True)
+#         # Tokenize text inputs
+#         text_input_1 = self.tokenizer.batch_encode_plus([text_1], max_length= self.source_len, pad_to_max_length=True,return_tensors='pt', truncation=True)
+#         text_input_2 = self.tokenizer.batch_encode_plus([text_2], max_length= self.source_len, pad_to_max_length=True,return_tensors='pt', truncation=True)
+#         target = self.tokenizer.batch_encode_plus([label], max_length= self.summ_len, pad_to_max_length=True,return_tensors='pt', truncation=True)
 
 
-        input_ids_1 = text_input_1['input_ids'].squeeze()
-        attention_mask_1 = text_input_1['attention_mask'].squeeze()
-        input_ids_2 = text_input_2['input_ids'].squeeze()
-        attention_mask_2 = text_input_2['attention_mask'].squeeze()
+#         input_ids_1 = text_input_1['input_ids'].squeeze()
+#         attention_mask_1 = text_input_1['attention_mask'].squeeze()
+#         input_ids_2 = text_input_2['input_ids'].squeeze()
+#         attention_mask_2 = text_input_2['attention_mask'].squeeze()
 
-        # print(input_ids_1, input_ids_2)
+#         # print(input_ids_1, input_ids_2)
 
-        target_ids = target['input_ids'].squeeze()
-        target_mask = target['attention_mask'].squeeze()
+#         target_ids = target['input_ids'].squeeze()
+#         target_mask = target['attention_mask'].squeeze()
 
-        # input_ids = torch.cat((input_ids_1, input_ids_2), dim=1)
-        # attention_mask = torch.cat((attention_mask_1, attention_mask_2), dim=1)
+#         # input_ids = torch.cat((input_ids_1, input_ids_2), dim=1)
+#         # attention_mask = torch.cat((attention_mask_1, attention_mask_2), dim=1)
 
-        return {
-            'input_ids_1': input_ids_1.to(dtype=torch.long), 
-            'attention_mask_1': attention_mask_1.to(dtype=torch.long), 
-            'input_ids_2': input_ids_2.to(dtype=torch.long), 
-            'attention_mask_2': attention_mask_2.to(dtype=torch.long), 
-            'target_ids': target_ids.to(dtype=torch.long),
-            'target_ids_y': target_ids.to(dtype=torch.long),
-            'bugid': torch.tensor(self.bugid[index], dtype=torch.long),
-            'bug': self.bug[index]
-        }
+#         return {
+#             'input_ids_1': input_ids_1.to(dtype=torch.long), 
+#             'attention_mask_1': attention_mask_1.to(dtype=torch.long), 
+#             'input_ids_2': input_ids_2.to(dtype=torch.long), 
+#             'attention_mask_2': attention_mask_2.to(dtype=torch.long), 
+#             'target_ids': target_ids.to(dtype=torch.long),
+#             'target_ids_y': target_ids.to(dtype=torch.long),
+#             'bugid': torch.tensor(self.bugid[index], dtype=torch.long),
+#             'bug': self.bug[index]
+#         }
 
-def semantic_training(generator, gen_opt, gen_tokenizer, adv_loader, device,epoch):
+# def semantic_training(generator, gen_opt, gen_tokenizer, adv_loader, device,epoch):
 
-    generator.train()
+#     generator.train()
     
-    for _,data in enumerate(adv_loader, 0):
-        y = data['target_ids'].to(device, dtype = torch.long)
-        y_ids = y[:, :-1].contiguous()
-        lm_labels = y[:, 1:].clone().detach()
-        lm_labels[y[:, 1:] == gen_tokenizer.pad_token_id] = -100
+#     for _,data in enumerate(adv_loader, 0):
+#         y = data['target_ids'].to(device, dtype = torch.long)
+#         y_ids = y[:, :-1].contiguous()
+#         lm_labels = y[:, 1:].clone().detach()
+#         lm_labels[y[:, 1:] == gen_tokenizer.pad_token_id] = -100
 
-        input_ids_1 = data['input_ids_1'].to(device, dtype = torch.long)
-        attention_mask_1 = data['attention_mask_1'].to(device, dtype = torch.long)
-        input_ids_2 = data['input_ids_2'].to(device, dtype = torch.long)
-        attention_mask_2 = data['attention_mask_2'].to(device, dtype = torch.long)
+#         input_ids_1 = data['input_ids_1'].to(device, dtype = torch.long)
+#         attention_mask_1 = data['attention_mask_1'].to(device, dtype = torch.long)
+#         input_ids_2 = data['input_ids_2'].to(device, dtype = torch.long)
+#         attention_mask_2 = data['attention_mask_2'].to(device, dtype = torch.long)
 
-        input_ids = torch.cat((input_ids_1, input_ids_2), dim=1)
-        attention_mask = torch.cat((attention_mask_1, attention_mask_2), dim=1)
+#         input_ids = torch.cat((input_ids_1, input_ids_2), dim=1)
+#         attention_mask = torch.cat((attention_mask_1, attention_mask_2), dim=1)
         
-        # ids = data['source_ids'].to(device, dtype = torch.long)
-        # mask = data['source_mask'].to(device, dtype = torch.long)
-        bugid = data['bugid'].to(device, dtype = torch.long)
-        bug = data['bug']
-        # print(f'bugid: {bugid}')
+#         # ids = data['source_ids'].to(device, dtype = torch.long)
+#         # mask = data['source_mask'].to(device, dtype = torch.long)
+#         bugid = data['bugid'].to(device, dtype = torch.long)
+#         bug = data['bug']
+#         # print(f'bugid: {bugid}')
         
                 
-        bugcode = input_ids_2[0]
-        end_index=getEndIndex(bugcode,32108) #2625 is the index for 'context',32108 is the index of 'context:'       
-        bugcode = bugcode[3:end_index-1] #your index may be different!
-        buggy = [gen_tokenizer.decode(bugcode, skip_special_tokens=True, clean_up_tokenization_spaces=True)]
+#         bugcode = input_ids_2[0]
+#         end_index=getEndIndex(bugcode,32108) #2625 is the index for 'context',32108 is the index of 'context:'       
+#         bugcode = bugcode[3:end_index-1] #your index may be different!
+#         buggy = [gen_tokenizer.decode(bugcode, skip_special_tokens=True, clean_up_tokenization_spaces=True)]
            
             
-        outputs = generator(input_ids = input_ids, attention_mask = attention_mask, decoder_input_ids=y_ids, labels=lm_labels)
-        loss = outputs[0]
-        # print(f'original loss: {loss}')
-        lm_logits = outputs[1]
-        output = F.log_softmax(lm_logits, -1)
-        preds_seq = output.max(2)[1]
+#         outputs = generator(input_ids = input_ids, attention_mask = attention_mask, decoder_input_ids=y_ids, labels=lm_labels)
+#         loss = outputs[0]
+#         # print(f'original loss: {loss}')
+#         lm_logits = outputs[1]
+#         output = F.log_softmax(lm_logits, -1)
+#         preds_seq = output.max(2)[1]
 
-        g = preds_seq[0]
-        end_index=getEndIndex(g,1)            
-        g = g[:end_index]      
+#         g = preds_seq[0]
+#         end_index=getEndIndex(g,1)            
+#         g = g[:end_index]      
 
 
 
-        preds = [gen_tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True)]
-        predstr = preds[0] 
-        print(f'predstr: {predstr}')
+#         preds = [gen_tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True)]
+#         predstr = preds[0] 
+#         print(f'predstr: {predstr}')
 
-        # identity discriminator
-        identity_reward = identity_discriminator(buggy[0], predstr)
+#         # identity discriminator
+#         identity_reward = identity_discriminator(buggy[0], predstr)
 
-        reward = autograd.Variable(torch.FloatTensor([1.0]))
+#         reward = autograd.Variable(torch.FloatTensor([1.0]))
 
-        if 'same' in identity_reward:
-            reward = autograd.Variable(torch.FloatTensor([1.4]))
-        else:            
-            reward = validate_by_compiler(bugid, predstr, bug)
+#         if 'same' in identity_reward:
+#             reward = autograd.Variable(torch.FloatTensor([1.4]))
+#         else:            
+#             reward = validate_by_compiler(bugid, predstr, bug)
         
-        print(f'reward: {reward}')
+#         print(f'reward: {reward}')
   
-        #combine cross entropy loss and compiler reward loss
-        reward = reward.to(device)   
-        loss = outputs[0]*reward
-        print(f'semantic loss: {loss}')
+#         #combine cross entropy loss and compiler reward loss
+#         reward = reward.to(device)   
+#         loss = outputs[0]*reward
+#         print(f'semantic loss: {loss}')
 
-        gen_opt.zero_grad()
-        loss.backward()
-        gen_opt.step()        
+#         gen_opt.zero_grad()
+#         loss.backward()
+#         gen_opt.step()        
 
-        recordData(epoch, bugid.item(), outputs[0].item(), reward.item(), predstr )             
+#         recordData(epoch, bugid.item(), outputs[0].item(), reward.item(), predstr )             
 
-def recordData(epoch, bugid, crossEntropLoss, reward, preds):
-    with open('./logs.csv', 'a') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter='\t',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow([epoch, bugid, crossEntropLoss, reward, preds])   
+# def recordData(epoch, bugid, crossEntropLoss, reward, preds):
+#     with open('./logs.csv', 'a') as csvfile:
+#         filewriter = csv.writer(csvfile, delimiter='\t',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+#         filewriter.writerow([epoch, bugid, crossEntropLoss, reward, preds])   
 
                 
-def getEndIndex(g,index):
-    end_index=0
-    for i in g:
-        end_index+=1
-        # 1 for </s>
-        if i == index:
-            break
-    return end_index
+# def getEndIndex(g,index):
+#     end_index=0
+#     for i in g:
+#         end_index+=1
+#         # 1 for </s>
+#         if i == index:
+#             break
+#     return end_index
                 
                 
-def identity_discriminator(buggy, predstr):
-    print(f'buggy: {buggy}')
-    print(f'predstr: {predstr}')
-    if buggy in predstr and predstr in buggy:
-        return 'same'
-    else:
-        return 'different'
+# def identity_discriminator(buggy, predstr):
+#     print(f'buggy: {buggy}')
+#     print(f'predstr: {predstr}')
+#     if buggy in predstr and predstr in buggy:
+#         return 'same'
+#     else:
+#         return 'different'
       
        
     
-def validate_by_compiler(bugid, preds, bug):
-    R = 0.2
-    result = BugsPHPDiscriminator.getResults(bugid.item(), preds, rootPath, bug)
-    print(f'result: {result}')
-    if 'noTestResults' in result:
-        rewardValue=1+R
-    elif 'failedFailingTests' in result:
-        rewardValue=1-R
-    elif 'passedFailingTests' in result:
-        rewardValue=1-R*2
-    elif 'passAllTests' in result:
-        rewardValue=1-R*3
-    else:
-        rewardValue=1
+# def validate_by_compiler(bugid, preds, bug):
+#     R = 0.2
+#     result = BugsPHPDiscriminator.getResults(bugid.item(), preds, rootPath, bug)
+#     print(f'result: {result}')
+#     if 'noTestResults' in result:
+#         rewardValue=1+R
+#     elif 'failedFailingTests' in result:
+#         rewardValue=1-R
+#     elif 'passedFailingTests' in result:
+#         rewardValue=1-R*2
+#     elif 'passAllTests' in result:
+#         rewardValue=1-R*3
+#     else:
+#         rewardValue=1
         
     
-    return autograd.Variable(torch.FloatTensor([rewardValue]))
+#     return autograd.Variable(torch.FloatTensor([rewardValue]))
 
+
+# def getGeneratorDataLoader(filepatch,tokenizer,batchsize):
+#     df = pd.read_csv(filepatch,encoding='latin-1',delimiter='\t')
+#     print(df.head(1))
     
+#     df = df[['bugid','bug', 'buggy', 'additional_info','patch']]
+
+#     params = {
+#         'batch_size': batchsize,
+#         'shuffle': False,
+#         'num_workers': 0
+#         }
+
+#     dataset=df.reset_index(drop=True)
+#     target_set = GeneratorDatasetForMultiSource(dataset, tokenizer, MAX_LEN, PATCH_LEN)
+#     target_loader = DataLoader(target_set, **params)
+#     return target_loader
+        
+# def semantic(epoch):   
+    
+#     gen = T5ForMultiSourceConditionalGeneration.from_pretrained(SAVE_MODEL, output_hidden_states=True)    
+#     gen_tokenizer = T5Tokenizer.from_pretrained(SAVE_MODEL,truncation=True)
+#     gen = gen.to(device)   
+#     gen_optimizer = torch.optim.Adam(params = gen.parameters(), lr=LEARNING_RATE)
+#     data_loader=getGeneratorDataLoader(semantic_train_data_path,gen_tokenizer,1)   
+
+#     print('\n---Semantic Training-----\nEPOCH %d\n--------' % (epoch+1))
+
+#     # train model
+#     semantic_training(gen, gen_optimizer, gen_tokenizer, data_loader, device, epoch)                       
+#     # save trained model       
+#     gen.save_pretrained(SAVE_MODEL)
+#     gen_tokenizer.save_pretrained(SAVE_MODEL)  
+#     print(f'Sementic Train Model Saved: {epoch}')
+
 
 def syntrain(epoch, tokenizer, model, device, loader, optimizer):
     model.train()
@@ -271,49 +305,6 @@ def syntrain(epoch, tokenizer, model, device, loader, optimizer):
         # optimizer.step()
         
         
-        
-def valid( tokenizer, model, device, loader, optimizer):
-    model.eval()
-    total_loss = 0 
-    total_nb=0
-    with torch.no_grad():
-        for _,data in enumerate(loader, 0):
-            y = data['target_ids'].to(device, dtype = torch.long)
-            y_ids = y[:, :-1].contiguous()
-            lm_labels = y[:, 1:].clone().detach()
-            lm_labels[y[:, 1:] == tokenizer.pad_token_id] = -100
-            ids = data['source_ids'].to(device, dtype = torch.long)
-            mask = data['source_mask'].to(device, dtype = torch.long)
-
-            outputs = model(input_ids = ids, attention_mask = mask, decoder_input_ids=y_ids, labels=lm_labels)
-            loss = outputs[0]
-            total_nb += 1  
-            total_loss += loss.item()    
-
-        print(f'Total Loss:  {total_loss}/{total_nb}')
-        
-
-
-def getGeneratorDataLoader(filepatch,tokenizer,batchsize):
-    df = pd.read_csv(filepatch,encoding='latin-1',delimiter='\t')
-    print(df.head(1))
-    
-    df = df[['bugid','bug', 'buggy', 'additional_info','patch']]
-
-    params = {
-        'batch_size': batchsize,
-        'shuffle': False,
-        'num_workers': 0
-        }
-
-    dataset=df.reset_index(drop=True)
-    target_set = GeneratorDatasetForMultiSource(dataset, tokenizer, MAX_LEN, PATCH_LEN)
-    target_loader = DataLoader(target_set, **params)
-    return target_loader
-        
-
-
-
 def syntactic(epoch,syn_train_data_path):  
     # Set random seeds and deterministic pytorch for reproducibility
     torch.manual_seed(SEED) # pytorch random seed
@@ -364,28 +355,7 @@ def syntactic(epoch,syn_train_data_path):
     tokenizer.save_pretrained(SAVE_MODEL)
     print(f'Syntatic Train Model Saved: {epoch}')
 
-        
-        
-def semantic(epoch):   
-    
-    gen = T5ForMultiSourceConditionalGeneration.from_pretrained(SAVE_MODEL, output_hidden_states=True)    
-    gen_tokenizer = T5Tokenizer.from_pretrained(SAVE_MODEL,truncation=True)
-    gen = gen.to(device)   
-    gen_optimizer = torch.optim.Adam(params = gen.parameters(), lr=LEARNING_RATE)
-    data_loader=getGeneratorDataLoader(semantic_train_data_path,gen_tokenizer,1)   
-
-    print('\n---Semantic Training-----\nEPOCH %d\n--------' % (epoch+1))
-
-    # train model
-    semantic_training(gen, gen_optimizer, gen_tokenizer, data_loader, device, epoch)                       
-    # save trained model       
-    gen.save_pretrained(SAVE_MODEL)
-    gen_tokenizer.save_pretrained(SAVE_MODEL)  
-    print(f'Sementic Train Model Saved: {epoch}')
-
-                
-        
-        
+      
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
     device = 'cuda' if cuda.is_available() else 'cpu'
